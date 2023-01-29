8b5860f2032b8259e70aa0265c5e24598bf79e6b
Z-main <izeemeen@gmail.com>
2023-01-30 01:51:29 +0300

    Update test.cpp

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_all.hpp>
 #include <source.h>
 
-TEST_CASE("Basic") {
+TEST_CASE("Parsing") {
     std::vector<std::vector<std::string>> data_actual;
     std::vector<std::string> cells_actual;
 
@@ -14,4 +14,23 @@ TEST_CASE("Basic") {
     REQUIRE(cells_actual == cells_expected);
     REQUIRE(data_actual == data_expected);
 
+}
+
+TEST_CASE("Counting") {
+    std::vector<std::vector<std::string>> data_actual = {{"1", "1", "0", "1"}, {"2", "2", "=A1+Cell30", "0"}, {"30", "0", "=B1+A1", "5"}};
+    std::vector<std::string> cells_actual = {"", "A", "B", "Cell"};
+
+    for (int out = 0; out < data_actual.size(); ++out) {
+        for (int in = 0; in < data_actual[0].size(); ++in) {
+
+            if (data_actual[out][in][0] == '=') {
+
+                Convert(cells_actual, data_actual, out, in);
+            }
+        }
+    }
+
+    std::vector<std::vector<std::string>> data_expected = {{"1", "1", "0", "1"}, {"2", "2", "6", "0"}, {"30", "0", "1", "5"}};
+
+    REQUIRE(data_actual == data_expected);
 }
